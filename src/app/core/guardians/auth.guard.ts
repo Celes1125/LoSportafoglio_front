1c34f39d72bf4124d9f884a6ce569f1234b2d1d5
cc <colauttiadad@gmail.com>
2025-02-14 12:53:57 +0100

    fix(guardian)

--- src/app/core/guardians/auth.guard.ts
+++ src/app/core/guardians/auth.guard.ts
@@ -1,24 +1,21 @@
-
-import { CanActivateFn } from '@angular/router';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../services/authentication.service';
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-
-  const router: Router = new Router; 
-  const authService = inject(AuthenticationService)  
+  const router = inject(Router);
+  const authService = inject(AuthenticationService);
 
   if(authService.isLogged()){
     console.log('Usuario autenticado, acceso permitido');
-    return true
-  }else{    
+    return true;
+  } else {    
     console.log('Usuario no autenticado, redirigiendo a login');
-    router.navigateByUrl('/login')
-    return false
+    router.navigateByUrl('/login');
+    return false;
   }
-  
 };
+
 
 
 
